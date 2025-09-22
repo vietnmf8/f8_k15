6ba13c78232a8031831f01ce20c7d6ef77cb6d7d
vietnmf8 <vietnmf8@fullstack.edu.vn>
2025-09-22 22:24:48 +0700

    cap nhat tren lop

---
+++ Day_31/main.js
@@ -0,0 +1,20 @@
+/**
+ * Garbage Collection (Tự động dọn rác => tối ưu bộ nhớ)
+ * ? Nếu không dọn dẹp
+ * => Memory leak - Rò rỉ bộ nhớ => Chậm/Crash App
+ * 
+ * JavaScript - Ngôn ngữ lập trình bậc cao
+ * + Bậc thấp: Gần với máy hơn
+ * + Bậc cao : Gần với ngôn ngữ tự nhiên
+ */
+
+
+/**
+ * Closure (Bao đóng)
+ * Tính chất tự nhiên/mặc định của hàm trong JS mà chúng ta không can thiệp được!!!
+ * 
+ * => Closures nhắc tới việc mỗi hàm sẽ luôn nhớ được phạm vi nó được định nghĩa, closure nhớ được mọi biến/hàm được khai báo ở phạm vi bên ngoài nó, dù phạm vi đó không còn tồn tại
+ */
+
+
+Promise.try
